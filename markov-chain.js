5391ce6655fbb92bcffe450238a9d58f785fb985
pomber <pombopombopombo@gmail.com>
2017-02-21 20:21:16 -0300

    Add create and update functions

--- markov-chain.js
+++ markov-chain.js
@@ -1,10 +1,36 @@
 (function() {
-  if (typeof markov === "undefined" || markov === null) {
-    var markov = {};
+  var START = " ^";
+  var END = " $";
+
+  function add(model, prev, token) {
+    model.count += 1;
+
+    model.tokens[token] = model.tokens[token] || { count: 0, tokens: {} };
+    model.tokens[token].count += 1;
+
+    if (prev) {
+      model.tokens[prev] = model.tokens[prev] || { count: 0, tokens: {} };
+      model.tokens[prev].tokens[token] = model.tokens[prev].tokens[token] ||
+        { count: 0, tokens: {} };
+      model.tokens[prev].tokens[token].count += 1;
+    }
   }
 
-  markov.foo = function() {
-    return "yes";
+  function update(model, tokens) {
+    var prev = null;
+    [].concat(START, tokens, END).forEach(function(token) {
+      add(model, prev, token);
+      prev = token;
+    });
+  }
+
+  function create() {
+    return { count: 0, tokens: {} };
+  }
+
+  var markov = {
+    create: create,
+    update: update
   };
 
   if (typeof module != "undefined" && module.exports) {
