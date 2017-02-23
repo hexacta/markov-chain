389696b682a1363b9050fff525e0463635efe151
pomber <pombopombopombo@gmail.com>
2017-02-23 11:20:35 -0300

    Refactor

--- markov-chain.js
+++ markov-chain.js
@@ -2,35 +2,42 @@
   var START = " ^";
   var END = " $";
 
-  function count(model, token) {
-    const tmodel = model.tokens[token];
-    return tmodel ? tmodel.count : 0;
+  function create() {
+    return { count: 0, tokens: {} };
   }
 
-  function add(model, prev, token) {
-    model.count += 1;
-
-    model.tokens[token] = model.tokens[token] || { count: 0, tokens: {} };
-    model.tokens[token].count += 1;
-
-    if (prev) {
-      model.tokens[prev] = model.tokens[prev] || { count: 0, tokens: {} };
-      model.tokens[prev].tokens[token] = model.tokens[prev].tokens[token] ||
-        { count: 0, tokens: {} };
-      model.tokens[prev].tokens[token].count += 1;
-    }
-  }
-
-  function update(model, tokens) {
+  function update(model, chain) {
     var prev = null;
-    [].concat(START, tokens, END).forEach(function(token) {
-      add(model, prev, token);
+    [].concat(START, chain, END).forEach(function(token) {
+      add(model, [prev, token]);
       prev = token;
     });
   }
 
-  function create() {
-    return { count: 0, tokens: {} };
+  function add(model, chain) {
+    var node = getNode(model, chain);
+    node.count += 1;
+    var next = chain.slice(1);
+
+    if (next.length) {
+      add(model, next);
+    } else {
+      model.count += 1;
+    }
+  }
+
+  function getNode(model, chain) {
+    var node = model;
+    chain.forEach(function(token) {
+      node.tokens[token] = node.tokens[token] || create();
+      node = node.tokens[token];
+    });
+    return node;
+  }
+
+  function count(model, chain) {
+    var node = getNode(model, chain);
+    return node.count;
   }
 
   var markov = {
--- markov-chain.test.js
+++ markov-chain.test.js
@@ -8,13 +8,27 @@ test("create", t => {
 test("one token", t => {
   const model = markov.create();
   markov.update(model, ["one"]);
-  const count = markov.count(model, "one");
+  const count = markov.count(model, ["one"]);
   t.is(count, 1);
 });
 
 test("missing token", t => {
   const model = markov.create();
   markov.update(model, ["one"]);
-  const count = markov.count(model, "two");
+  const count = markov.count(model, ["two"]);
   t.is(count, 0);
+});
+
+test("a b c", t => {
+  const model = markov.create();
+  markov.update(model, ["a", "b", "c"]);
+
+  t.is(markov.count(model, ["a"]), 1);
+  t.is(markov.count(model, ["b"]), 1);
+  t.is(markov.count(model, ["c"]), 1);
+  t.is(markov.count(model, ["d"]), 0);
+  t.is(markov.count(model, ["a", "b"]), 1);
+  t.is(markov.count(model, ["b", "c"]), 1);
+  t.is(markov.count(model, ["a", "c"]), 0);
+  t.is(markov.count(model, ["a", "b", "c"]), 1);
 });
